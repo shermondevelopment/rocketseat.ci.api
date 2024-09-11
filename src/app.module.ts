438603bc2bd6548f2f1e565dab3985d1744e45d8
shermondevelopment <victorshermon@yahoo.com>
2024-09-10 21:35:45 -0300

    fix: commit bank

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'mysql',
-      port: 3306,
-      username: 'admin',
-      password: 'root',
-      database: 'rocketseat-db',
-      entities: [],
-      synchronize: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'mysql',
+    //   port: 3306,
+    //   username: 'admin',
+    //   password: 'root',
+    //   database: 'rocketseat-db',
+    //   entities: [],
+    //   synchronize: true,
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
