b2d65c209962e028dd56a70aec5bb6a3fe746032
shermondevelopment <victorshermon@yahoo.com>
2024-09-10 21:49:25 -0300

    minor fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Olá, Rocketseat!';
+    return 'Olá, vitorshermon!';
   }
 }
