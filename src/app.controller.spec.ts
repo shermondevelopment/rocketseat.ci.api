e0e922f89e80519af3fe1e7d228a78a1f7405813
shermondevelopment <victorshermon@yahoo.com>
2024-09-13 21:52:33 -0300

    check test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Olá, vitorshermon!');
+      expect(appController.getHello()).toBe('Olá, meu parceiro!');
     });
   });
 });
